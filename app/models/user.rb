e73abe5dd43b6be88860bc080d8f8ce9906902d2
nana <sss1230871997@gmail.com>
2024-08-22 10:06:59 +0900

    re

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :tasks, dependent: :destroy
-  validates :name, presence: true, length: { maximum: 255 }
-  validates :email, uniqueness: true, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: {minimum: 6 }
-  validates_confirmation_of :password
+  validates :password_confirmation, presence: true
   before_validation { email.downcase! }
 
 end
