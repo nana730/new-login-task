d461e2db1875a92877021d7bd4878318b1478bbd
nana <sss1230871997@gmail.com>
2024-08-22 15:28:11 +0900

    yarinaosi5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :email, presence: true, uniqueness: { message: "はすでに使用されています" }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: {minimum: 6 }
-  validates :password_confirmation, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
   before_validation { email.downcase! }
 
 end
