ec09181f491537768e1ee9586ef0ce2d89c9e637
nana <sss1230871997@gmail.com>
2024-08-22 11:50:16 +0900

    doko

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :tasks
-  validates :name, presence: { message: '名前を入力してください'}
-  validates :email, presence: { message: 'メールアドレスを入力してください'}
-  validates :email, uniqueness: {message: 'メールアドレスはすでに使用されています'}
-  validates :password, presence: {message: 'メールアドレスを 入力してください'},
-  length: {minimum: 6, message: 'パスワードは6文字以上で入力してください'}
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true, confirmation: { message: 'パスワード（確認）とパスワードの入力が一致しません' }
 
 end
